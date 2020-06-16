4ea76edb0823be4f42f2988511bbea114822f525
rhensshuya23 <abdullah@bywave.com.au>
2020-06-16 18:31:12 +0800

    Fixing adminLTE UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
 <div class="container">
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
+ 
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
