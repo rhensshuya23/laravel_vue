2a38c447c3b08c36c299714ce03614408efb9fb8
rhensshuya23 <abdullah.devexpert@gmail.com>
2020-06-19 14:11:57 +0800

    Move title to class card-title

--- resources/js/components/Developer.vue
+++ resources/js/components/Developer.vue
@@ -3,13 +3,13 @@
         <div class="row mt-5">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Developer Component</div>
-
-                    <div class="card-body">
-                        <passport-clients></passport-clients>
-                        <passport-authorized-clients></passport-authorized-clients>
-                        <passport-personal-access-tokens></passport-personal-access-tokens>
-                    </div>
+                    <div class="card-header"></div>
+                        <div class="card-title">Developer Component</div>
+                        <div class="card-body">
+                            <passport-clients></passport-clients>
+                            <passport-authorized-clients></passport-authorized-clients>
+                            <passport-personal-access-tokens></passport-personal-access-tokens>
+                        </div>
                 </div>
             </div>
         </div>
