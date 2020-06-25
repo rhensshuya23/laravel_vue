483006cd89e729360c31ecd836b4ab662d837772
rhensshuya23 <abdullah.devexpert@gmail.com>
2020-06-25 21:44:36 +0800

    fixed dashboard.vue

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -4,7 +4,7 @@
           <div class="container-fluid">
             <div class="row mb-2">
               <div class="col-sm-6">
-                <h5 class="m-0 text-dark">Don't stop to push yourself, everyday is a new beggining!
+                <h5 class="m-0 text-dark">Don't stop to push yourself, everyday is a new beggining!</h5>
               </div><!-- /.col -->
             </div><!-- /.row -->
 
